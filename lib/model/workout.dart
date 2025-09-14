bf4757bf5723290791054bcdf33dbb78c290e3b2
wkh11 <whe14@u.rochester.edu>
2025-09-14 17:56:02 -0400

    create map for enum, bodygroupparts

--- lib/model/workout.dart
+++ lib/model/workout.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/widgets.dart';
 import 'package:uuid/uuid.dart';
+import 'package:flutter/material.dart';
 
 const uuid = Uuid();
 
 enum BodyGroupCategory { arms, chest, legs, back, shoulders }
+
+//need to learn how to create custom icons
+Map<BodyGroupCategory, IconData> bodyGroupMap = {
+  BodyGroupCategory.arms: Icons.accessibility_new,
+  BodyGroupCategory.back: Icons.sports_basketball,
+  BodyGroupCategory.chest: Icons.sports_gymnastics,
+  BodyGroupCategory.legs: Icons.sports_football,
+  BodyGroupCategory.shoulders: Icons.sports_baseball,
+};
 
 class Workout {
   Workout({
